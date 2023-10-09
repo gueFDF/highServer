4908e515369264e5c329a22964b55a91a3384292
gueFDF <3237455241@qq.com>
2023-10-09 11:48:53 +0800

    fixbug: 为logging 加入namespace 不然nanespace外无法使用

--- include/asynLog/Logging.h
+++ include/asynLog/Logging.h
@@ -60,21 +60,21 @@ inline Logger::LogLevel Logger::logLevel() {
 }
 
 // 日志宏
-#define LOG_TRACE                            \
-    if (Logger::logLevel() <= Logger::TRACE) \
-    Logger(__FILE__, __LINE__, Logger::TRACE, __func__).stream()
-#define LOG_DEBUG                            \
-    if (Logger::logLevel() <= Logger::DEBUG) \
-    Logger(__FILE__, __LINE__, Logger::DEBUG, __func__).stream()
-#define LOG_INFO                            \
-    if (Logger::logLevel() <= Logger::INFO) \
-    Logger(__FILE__, __LINE__).stream()
+#define LOG_TRACE                                                    \
+    if (highServer::Logger::logLevel() <= highServer::Logger::TRACE) \
+    highServer::Logger(__FILE__, __LINE__, highServer::Logger::TRACE, __func__).stream()
+#define LOG_DEBUG                                                    \
+    if (highServer::Logger::logLevel() <= highServer::Logger::DEBUG) \
+    highServer::Logger(__FILE__, __LINE__, highServer::Logger::DEBUG, __func__).stream()
+#define LOG_INFO                                                    \
+    if (highServer::Logger::logLevel() <= highServer::Logger::INFO) \
+    highServer::Logger(__FILE__, __LINE__).stream()
 
-#define LOG_WARN Logger(__FILE__, __LINE__, Logger::WARN).stream()
-#define LOG_ERROR Logger(__FILE__, __LINE__, Logger::ERROR).stream()
-#define LOG_FATAL Logger(__FILE__, __LINE__, Logger::FATAL).stream()
-#define LOG_SYSERR Logger(__FILE__, __LINE__, false).stream()
-#define LOG_SYSFATAL Logger(__FILE__, __LINE__, true).stream()
+#define LOG_WARN highServer::Logger(__FILE__, __LINE__, highServer::Logger::WARN).stream()
+#define LOG_ERROR highServer::Logger(__FILE__, __LINE__, highServer::Logger::ERROR).stream()
+#define LOG_FATAL highServer::Logger(__FILE__, __LINE__, highServer::Logger::FATAL).stream()
+#define LOG_SYSERR highServer::Logger(__FILE__, __LINE__, false).stream()
+#define LOG_SYSFATAL highServer::Logger(__FILE__, __LINE__, true).stream()
 
 } // namespace highServer
 #endif /* LOGGING */
